10b425d8448e4d80300da949309ec231c02c4e2a
krichevets <dkrich99@gmail.com>
2023-04-22 17:00:45 +0300

    Add project files.

---
+++ A/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader reader = File.OpenText("D:/рабочий стол/Kontest/Kontest/A/input.txt");
+            StreamReader reader = File.OpenText("input.txt");
+            string line = "";
+            var intList = new List<BigInteger>();
+            BigInteger outPutNum = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string concat = String.Concat(line.Where(symbol => Char.IsNumber(symbol)));
+                if (concat.Length > 0)
+                {
+                    BigInteger curInt = BigInteger.Parse(concat);
+
+                    intList.Add(curInt);
+                    outPutNum = outPutNum + curInt;
+                }
+            }
+
+            var outPutCharArr = outPutNum.ToString().ToCharArray();
+
+            string output = "[";
+
+            for (var i = 0; i < outPutCharArr.Length; i++)
+            {
+                output = output + outPutCharArr[i];
+                if (i != outPutCharArr.Length - 1)
+                {
+                    output = output + ", ";
+                }
+            }
+
+            output = output + "]";
+
+            //Console.Write(output);
+            //File.WriteAllText("D:/рабочий стол/Kontest/Kontest/A/output.txt", output);
+            File.WriteAllText("output.txt", output);
+
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ B/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B
+{
+    struct Schedule
+    {
+        public DateTime From;
+        public DateTime To;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = File.OpenText("input.txt");
+            var lines = reader.ReadToEnd().Split("\r\n");
+            var countNumbers = 0;
+            var countMeetings = 0;
+            var mySchedule = new Schedule();
+            List<Schedule> teamSchedules = new List<Schedule>();
+            for (var i = 0; i < lines.Length; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        var data = lines[i].Split(" ").Select(t => Convert.ToInt32(t)).ToArray();
+                        countNumbers = data[0];
+                        countMeetings = data[1];
+                        break;
+                    case 1:
+                        var range = lines[i].Split("-");
+                        mySchedule.From = DateTime.ParseExact(range[0], "HH:mm", null);
+                        mySchedule.To = DateTime.ParseExact(range[1], "HH:mm", null);
+                        break;
+                    default:
+                        var range2 = lines[i].Split(" ");
+                        var dateRange = range2[1].Split("-");
+                        var schedule = new Schedule()
+                        {
+                            From = DateTime.ParseExact(dateRange[0], "HH:mm", null),
+                            To = DateTime.ParseExact(dateRange[1], "HH:mm", null)
+                        };
+                        teamSchedules.Add(schedule);
+                        break;
+                }
+
+                teamSchedules = teamSchedules.OrderBy(t => t.To).ToList();
+                foreach (var schedule in teamSchedules)
+                {
+                    if (schedule.To >= mySchedule.To && schedule.To <= schedule.From)
+                    {
+                        Console.WriteLine("ALARM");
+                        continue;
+                    }
+
+                    var meetTime = new Schedule()
+                    {
+                        From = mySchedule.From.Add(schedule.To.TimeOfDay.Subtract(mySchedule.From.TimeOfDay)),
+                        To = mySchedule.To
+                    };
+                    Console.WriteLine(meetTime.From.TimeOfDay);
+                }
+            }
+        }
+    }
+}
